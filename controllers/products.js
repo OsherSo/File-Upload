745fb27fc9a43e64ea5fabc2a06a78f001e2defa
Osher Solimany <solimani.osher@gmail.com>
2023-12-03 22:04:31 +0200

    Add additional checks

--- controllers/products.js
+++ controllers/products.js
@@ -2,6 +2,7 @@ const path = require('path');
 const { StatusCodes } = require('http-status-codes');
 
 const Product = require('../models/Product');
+const { BadRequestError } = require('../errors');
 
 const getAllProducts = async (req, res) => {
   const products = await Product.find({});
@@ -19,7 +20,15 @@ const createProduct = async (req, res) => {
 };
 
 const uploadProductImage = async (req, res) => {
+  if (!req.files) throw new BadRequestError('No File Uploaded');
+
   const productImage = req.files.image;
+  if (!productImage.mimetype.startsWith('image'))
+    throw new BadRequestError('Please Upload Image');
+  const maxSize = 1024 * 1024;
+  if (productImage.size > maxSize)
+    throw new BadRequestError('PLease upload image smaller 1KB');
+
   const name = productImage.name;
   const imagePath = path.join(__dirname, '../public/uploads/' + `${name}`);
   await productImage.mv(imagePath);
